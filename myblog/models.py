e2b2d32f1569851931750dade4caabab75923ac0
csubedi111 <chandansubedi111@gmail.com>
2023-08-15 19:04:23 +0545

    post author fixed

--- myblog/models.py
+++ myblog/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 
-class BlogModel(models.Model):
+class BlogModel(models.Model): 
     title = models.CharField(max_length=100)
     content = models.TextField()
     author = models.ForeignKey(User,on_delete=models.CASCADE)
